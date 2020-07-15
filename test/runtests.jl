42018ebd3b65d35bee0b7f8a36281865d11aa943
Michael Hatherly <michaelhatherly@gmail.com>
2020-07-15 12:03:16 +0100

    Add traverse tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,34 @@ using TreeSitter, Test
             end
         end
     end
+    @testset "Tree Traversal" begin
+        p = Parser(:julia)
+        tree = parse(p, "[1, 2]")
+
+        out = String[]
+        traverse(tree) do node, enter
+            enter && push!(out, TreeSitter.node_type(node))
+        end
+        @test out == ["source_file", "array_expression", "[", "number", ",", "number", "]"]
+
+        out = String[]
+        traverse(tree) do node, enter
+            enter || push!(out, TreeSitter.node_type(node))
+        end
+        @test out == ["[", "number", ",", "number", "]", "array_expression", "source_file"]
+
+        out = String[]
+        traverse(tree, named_children) do node, enter
+            enter && push!(out, TreeSitter.node_type(node))
+        end
+        @test out == ["source_file", "array_expression", "number", "number"]
+
+        out = String[]
+        traverse(tree, named_children) do node, enter
+            enter || push!(out, TreeSitter.node_type(node))
+        end
+        @test out == ["number", "number", "array_expression", "source_file"]
+    end
     @testset "Languages" begin
         @testset "bash" begin
             p = Parser(:bash)
